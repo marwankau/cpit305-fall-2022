60950971c21bdd41ba18c81142b2b3193ba6b110
FaisalAlmadafei <113466869+FaisalAlmadafei@users.noreply.github.com>
2022-10-20 09:15:31 +0300

    uPDATED

--- src/lecture04/binaryexm/WriteBinary.java
+++ src/lecture04/binaryexm/WriteBinary.java
@@ -1,49 +1,58 @@
-package lecture04.binaryexm;
-
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
-public class WriteBinary {
-    public static void main(String[] args) throws IOException {
-        FileOutputStream fos = new FileOutputStream("src/lecture04/binaryexm/sample.txt");
-        DataOutputStream dos = new DataOutputStream(fos);
+public class App {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("data/source.csv"))) {
 
-
-        dos.writeInt(125);
-        // write fixed string with len 15 
-
-        byte[] fixed_name = new byte[15];
-        String name = "Ahmed";
-        
-        writeFixedString(name, fixed_name, 15);
-
-        dos.write(fixed_name);
-        dos.writeDouble(4.5);
+            FileOutputStream f = new FileOutputStream("data/data.db");
+          //  RandomAccessFile raf = new RandomAccessFile(f, "rw");
+          DataOutputStream D = new DataOutputStream(f) ;
+            String line = reader.readLine(); // skip column title (first line)
 
 
-        // Khalid
-        dos.writeInt(456);
-        name = "Khalid Ahmed Ali";
-        writeFixedString(name, fixed_name, 15);
-        dos.write(fixed_name);
-        dos.writeDouble(4.8);
+            while ((line = reader.readLine()) != null) {
+                Scanner scanner = new Scanner(line);
+                scanner.useDelimiter(",");
 
-        // Saeed
-        dos.writeInt(798);
-        name = "Saeed";
-        writeFixedString(name, fixed_name, 15);
-        dos.write(fixed_name);
-        dos.writeDouble(4.3);
+                int year = scanner.nextInt();
+                D.writeInt(year);
 
-        // Fahad
-        dos.writeInt(132);
-        name = "Fahad";
-        writeFixedString(name, fixed_name, 15);
-        dos.write(fixed_name);
-        dos.writeDouble(2.8);
+                String industryAggregation = scanner.next();
+                byte b[] = new byte [10] ; 
+                writeFixedString(industryAggregation, b,10);
+                D.write(b);
 
-        dos.close();
+
+                String industryCode = scanner.next();
+                byte b2[] = new byte [8] ; 
+                 writeFixedString(industryCode, b2 , 8);
+                 D.write(b2);
+
+
+                String industryName = scanner.next();
+                byte b3[] = new byte[50] ;
+                 writeFixedString(industryName, b3, 50);
+                 D.write(b3);
+
+                double value = scanner.nextDouble();
+                D.writeDouble(value);
+
+                System.out.printf("%5d %-10s %-10s %-50s %10.2f\n", year, industryAggregation, industryCode, industryName, value);
+
+            }
+
+            D.close();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
     private static void writeFixedString(String str, byte[] buff, int length) {
@@ -54,5 +63,7 @@ public class WriteBinary {
                 buff[i] = (byte)str.charAt(i);
             }
         }
+    
     }
+
 }
